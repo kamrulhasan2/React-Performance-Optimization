78a49ac0b3ee1694ed54d166896bfe08314c6406
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-10-07 18:53:55 +0600

    fix userProfile issue

--- src/components/Example-4/UserProfile.jsx
+++ src/components/Example-4/UserProfile.jsx
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 
 const UserProfile = ({ firstName, lastName }) => {
-    const [fullName, setFullName] = useState('');
+    // const [fullName, setFullName] = useState('');
 
-    useEffect(() => {
-        setFullName(`${firstName} ${lastName}`);
-    }, [firstName, lastName]);
+    // useEffect(() => {
+    //     setFullName(`${firstName} ${lastName}`);
+    // }, [firstName, lastName]);
+
+    // Here useEffect crate an extra side effect, so we can directly use the props 
+    // without useEffect and useState
+
+    const fullName = `${firstName} ${lastName}`;
 
     return <h1>Welcome, {fullName}</h1>;
 
